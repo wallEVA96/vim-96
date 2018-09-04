db0bc5e98f4cbfccec2dfadba470c8e2cccce038
walleva <walleva@email.com>
2018-09-03 20:13:21 -0700

    modify template by banner and figlet tool

--- .vim/bundle/vim-template/templates/=template=.c
+++ .vim/bundle/vim-template/templates/=template=.c
@@ -1,11 +1,16 @@
 /*
  * %FFILE%
+ *                        _ _                 
+ *         __      ____ _| | | _____   ____ _ 
+ *         \ \ /\ / / _` | | |/ _ \ \ / / _` |
+ *          \ V  V / (_| | | |  __/\ V / (_| |
+ *           \_/\_/ \__,_|_|_|\___| \_/ \__,_|
  * Copyright (C) %YEAR% %USER% <%MAIL%>
  *
  * Distributed under terms of the %LICENSE% license.
  */
 
-#include "%FILE%.h"
+#include ".h"
 
 
 %HERE%
